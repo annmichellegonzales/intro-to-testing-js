3ccd94e6295be244608b3496eb632a2724276d86
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2020-12-17 16:57:21 -0600

    feat: TDD coming along

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
-// function expression syntax (assigning an anonymous function to a variable)
-const helloWorld = function() {
+function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+    } else {
+        return "Hello, Jane!";}
+}
+
+console.log(sayHello("Alex"));
+console.log(sayHello("Pat"));
--- tests.js
+++ tests.js
@@ -9,8 +9,25 @@ describe('helloWorld', function() {
     it('should return the string "Hello, World!" when executed', function() {
       expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
+    it('should never return "undefined" when called', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
 
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" when executed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!")
+    });
+});
